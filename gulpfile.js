17531ececaba1c8f98c71ad0448995d7dbdb4d3d
Shusaku NAKAZATO <cu393uc@gmail.com>
2015-12-24 06:12:14 +0900

    added `metalsmith:clean` task with `del` module

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var del = require('del');
 
 var msConf = require('./metalsmith.config.js');
 
@@ -7,7 +8,14 @@ gulp.task('default', ['build']);
 
 gulp.task('build', ['metalsmith:build']);
 
-gulp.task('metalsmith:build', function () {
+gulp.task('metalsmith:build', ['metalsmith:clean'], function () {
   var ms = msConf.create('production');
   ms.build(function (err) { if (err) throw  err; });
+});
+
+gulp.task('metalsmith:clean', function () {
+  return del([
+      './build/**/*.html',
+      '!./build/assets/**/*'
+    ]);
 });
