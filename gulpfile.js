1b171e14898516defc7f9bfc69ae3998e7b6731b
Shusaku NAKAZATO <cu393uc@gmail.com>
2015-12-24 07:28:07 +0900

    introduced gulp-sass and gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ var gutil = require('gulp-util');
 var del = require('del');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 var msConf = require('./metalsmith.config.js');
 
 gulp.task('default', ['build']);
 
 gulp.task('build', [
+  'sass:build',
   'webpack:build',
   'metalsmith:build'
 ]);
@@ -35,5 +38,27 @@ gulp.task('metalsmith:clean', function () {
   return del([
       './build/**/*.html',
       '!./build/assets/**/*'
+    ]);
+});
+
+gulp.task('sass:build', ['css:clean'], function () {
+  gulp.src('./src/assets/sass/**/*.s[ac]ss')
+    .pipe(sass({
+      'outputStyle': 'nested'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./.tmp/css'))
+    .on('end', function () {
+      gulp.src('./.tmp/css/**/*.css')
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./build/assets'))
+        .on('end', function () {
+          del(['./.tmp/']);
+        });
+    });
+});
+
+gulp.task('css:clean', function () {
+  return del([
+      './build/assets/style.css'
     ]);
 });
