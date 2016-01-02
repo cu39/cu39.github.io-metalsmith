057b43ea57b7413b0861465af324f7ae9d1219f8
Shusaku NAKAZATO <cu393uc@gmail.com>
2016-01-02 12:53:36 +0900

    added remote url for grunt-build-controll
    ready to deploy!

--- gruntfile.js
+++ gruntfile.js
@@ -10,10 +10,11 @@ module.exports = function(grunt) {
         dir: 'build',
         commit: true,
         tag: false,
-        push: false
+        push: true
       },
       dist: {
         options: {
+          remote: 'git@github.com:cu39/cu39.github.io.git',
           branch: 'master'
         }
       }
